4534efef31fe31fe2258f61e6f261b7fc8db7bac
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-22 16:14:47 -0300

    created the receiveValue and convertValue functions and defined the click event button convert

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -10,11 +10,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        convertMlxG.setOnClickListener {
+        convertCAndF.setOnClickListener {
             check(true)
-            convertGxMl.isChecked = false}
-        convertGxMl.setOnClickListener {
+            convertFAndC.isChecked = false
+        }
+        convertFAndC.setOnClickListener {
             check(true)
-            convertMlxG.isChecked = false}
+            convertCAndF.isChecked = false
+        }
+
+        buttonConvert.setOnClickListener {
+            result.text = convertValue()
+        }
+    }
+
+    fun receiveValue(): Int {
+        val cap = textInput.text
+        return cap.toString().toInt()
+    }
+
+    fun convertValue(): String {
+        var result: Int = 0
+        var text: String = ""
+        if (convertCAndF.isChecked) {
+            result = ((receiveValue() * 1.8) + 32).toInt()
+            text = "${result}° F"
+        } else if (convertFAndC.isChecked) {
+            result = ((receiveValue() - 32) / 1.8).toInt()
+            text = "${result}° C"
+        }
+        return text
     }
 }

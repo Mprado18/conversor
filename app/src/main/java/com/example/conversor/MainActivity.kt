e17f8660bc8ea7d8fc216fc2b342fa4efabf836d
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-04-22 13:24:42 -0300

    set event radio button

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.conversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        convertMlxG.setOnClickListener {
+            check(true)
+            convertGxMl.isChecked = false}
+        convertGxMl.setOnClickListener {
+            check(true)
+            convertMlxG.isChecked = false}
     }
 }
